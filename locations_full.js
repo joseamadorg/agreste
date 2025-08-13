5504b9dd6bfb21e7f8a801b7d64df5e871b0eb89
amador_jose <amadorjose@gmail.com>
2025-08-13 18:06:36 -0400

    V2025 -AGOSTO- V.4.0.0 responsive, galeria, destinos, home, contacto y nosotros listo.

---
+++ locations_full.js
@@ -0,0 +1,26 @@
+const locations = [
+{
+        name: "Torres del Paine – Chile",
+        coords: [-50.9423262, -73.4093628],
+        categories: ["Ecoturismo", "Fotograf\u00eda"],
+        popupContent: `
+            <h3>Torres del Paine – Chile</h3>
+            <img src="https://agrestepatagonia.com/locations/wp-content/uploads/2025/06/AdobeStock_825514215-scaled-e1749491264905.jpeg" alt="Torres del Paine – Chile" class="popup-img"/>
+            <p>En el corazón del Parque Nacional Torres del Paine, en Chile, se encuentra una de las densidades más altas de pumas (Puma concolor puma) en el mundo —entre 3 y 7 individuos por cada 100 km²— gracias a su estricta protección y proyectos de convivencia con ganado local.</p>
+            <p><em>In the heart of Torres del Paine National Park in Chile lies one of the world's highest densities of pumas (Puma concolor puma)—between 3 and 7 individuals per 100 km²—thanks to strict protection and coexistence projects with local livestock.</em></p>
+            <a href="https://agrestepatagonia.com/locations/puma-torres-del-paine-chile/" target="_blank">+ Info</a>
+        `
+    },
+{
+        name: "Rancho Patagónico – Santa Cruz",
+        coords: [-47.6747698, -68.4648949],
+        categories: ["Ecoturismo", "Fotograf\u00eda"],
+        popupContent: `
+            <h3>Rancho Patagónico – Santa Cruz</h3>
+            <img src="https://agrestepatagonia.com/locations/wp-content/uploads/2025/07/361063536_3448042748780507_5247477360065200195_n-300x300.jpeg" alt="Rancho Patagónico – Santa Cruz" class="popup-img"/>
+            <p>Ubicado en una estancia de más de 10.700ha diseñada al estilo de los años 40, el Rancho Patagónico ofrece una experiencia día completo (o más) que combina cabalgatas, cocina tradicional, actividades culturales y exploración geológica en un entorno virgen.</p>
+            <p><em>Located on an estate of more than 10,700 hectares styled after the 1940s, Rancho Patagónico offers a full-day (or longer) experience that combines horseback riding, traditional cuisine, cultural activities, and geological exploration in a pristine setting.</em></p>
+            <a href="https://agrestepatagonia.com/locations/rancho-patagonico-santa-cruz/" target="_blank">+ Info</a>
+        `
+    }
+];
